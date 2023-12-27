8bdb499220d5a687b35116af6839a2eb985f60cd
mrzkhrmn <mrzkhrmn@hotmail..com>
2023-12-27 11:22:15 +0300

    some additional to userModel

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -5,6 +5,15 @@ const userSchema = mongoose.Schema(
     username: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    followers: { type: Array, required: true, default: [] },
+    following: { type: Array, required: true, default: [] },
+    profileImage: {
+      type: String,
+      required: true,
+      default:
+        "https://media.istockphoto.com/id/1495088043/vector/user-profile-icon-avatar-or-person-icon-profile-picture-portrait-symbol-default-portrait.jpg?s=612x612&w=0&k=20&c=dhV2p1JwmloBTOaGAtaA3AW1KSnjsdMt7-U_3EZElZ0=",
+    },
+    description: { type: Array, required: true, default: "" },
     isAdmin: { type: Boolean, required: true, default: false },
   },
   { timestamps: true }
